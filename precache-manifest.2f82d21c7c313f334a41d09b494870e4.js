68c6e2ec0babfae8195d81f2855541ddeef11586
KevenLiu <kevenliu94@gmail.com>
2020-03-26 17:58:56 -0400

    Updates

---
+++ precache-manifest.2f82d21c7c313f334a41d09b494870e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d47542b060efff3ab6f4b820e2af50",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8f5b709d9bc0b50845ef",
+    "url": "/monsters-rolodex/static/css/main.4ee1394b.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f5b709d9bc0b50845ef",
+    "url": "/monsters-rolodex/static/js/main.6e59318a.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
